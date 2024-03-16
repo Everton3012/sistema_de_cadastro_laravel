30343917d59ac1a8553e39ad3adc9fff151b7c00
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-03-16 02:22:16 -0300

    projeto finalizado

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <p class="mb-4 ">Olá <strong>{{ Auth::user()->name }}</strong></p>
+                </div>
+            </div>
+            <div class=" overflow-hidden shadow-sm sm:rounded-lg grid grid-cols-2 gap-4">
+                <div class="p-6 mt-2 rounded bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100">
+                    <p>Clientes cadastrados: {{ count($clientes)}}</p>
+                    <hr/>
+                    <p class="mt-4"><a href="{{ route('meus.clientes', Auth::user()->id )}}" class="bg-blue-500 hover:bg-blue-400 text-white rounded p-2">Ver Clientes</a></p>
+                </div>
+                <div class="p-6 mt-2 rounded bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100">
+                    <p>Usuarios cadastrados: {{ count($users)}}</p>
+                    <hr/>
+                    <p class="mt-4"><a href="{{ route('user.index')}}" class="bg-blue-500 hover:bg-blue-400 text-white rounded p-2">Ver Usuários</a></p>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Models\Cliente;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +23,11 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard',[
+        'clientes' => Cliente::all(),
+        'users' => User::all(),
+    ]);
+
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
